455a44a00d141300ee3de4ead01579807e1b26db
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-06-16 11:40:24 +0200

    Finalisation des exercices 1 à 5

--- __init__.py
+++ __init__.py
@@ -1,15 +1,53 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
+import json
 from datetime import datetime
 from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/tawarano')
+def tawarano():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+
+    raw_content = response.read()
+
+    json_content = json.loads(raw_content.decode('utf-8'))
+
+    results = []
+
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+
+    return jsonify(results=results)
+
+
+@app.route('/rapport')
+def rapport():
+    return render_template('graphique.html')
+
+
+@app.route('/histogramme')
+def histogramme():
+    return render_template('histogramme.html')
+
+
+@app.route('/exercices')
 def hello_world():
-    return render_template('hello.html')
-  
+    return render_template('exercices.html')
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
