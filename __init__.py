286db3c232a7aa9d3964c0cb0771b7b5ab722102
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-06-16 11:57:09 +0200

    Finalisation de l'exercice 6.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 import json
+from collections import Counter
+from datetime import datetime
 from urllib.request import urlopen
 
 from flask import Flask, render_template, jsonify
@@ -43,6 +45,26 @@ def rapport():
 def histogramme():
     return render_template('histogramme.html')
 
+@app.route('/commits/')
+def commits():
+    return render_template("commits.html")
+
+@app.route('/commits/data/')
+def commits_data():
+    response = urlopen('https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+
+    minutes_list = []
+    for commit in json_content:
+        date_str = commit.get('commit', {}).get('author', {}).get('date')
+        date_object = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        minutes_list.append(date_object.minute)
+
+    counter = Counter(minutes_list)
+    results = [{'minute': minute, 'count': count} for minute, count in sorted(counter.items())]
+
+    return jsonify(results=results)
 
 @app.route('/exercices')
 def hello_world():
